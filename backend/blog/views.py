173f50cd21e8a0d6e6f37631de25722d6bac5d5f
John <birukovie@gmail.com>
2023-12-05 16:23:12 +0300

    release v1

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework. permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
 
 from .models import Blog
 from .serializer import BlogSerializer
@@ -7,6 +8,8 @@ from .serializer import BlogSerializer
 
 class BlogViewSet(viewsets.ModelViewSet):
     serializer_class = BlogSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'content')
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
