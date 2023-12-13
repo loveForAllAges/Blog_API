5be329ae68ae2f5eb3d6c803de03fafcfc937fd2
John <birukovie@gmail.com>
2023-12-13 22:45:02 +0300

    upd

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -9,12 +9,12 @@ class TagSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class BlogSerializer(serializers.ModelSerializer):
+class BlogSerializer(serializers.HyperlinkedModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True, allow_empty=False)
 
     class Meta:
         model = Blog
-        fields = ('title', 'content', 'tags')
+        fields = ('title', 'content', 'tags', 'url')
 
     def create(self, validated_data):
         tags = validated_data.pop('tags')
